ad096d1d24a205c83f8c046cdc9167311310b7d3
André Corrêa <andre.correa.silva203@gmail.com>
2023-05-31 20:38:23 -0300

    add points in knowlodge base
    Co-authored-by: gabrielm2q <mariano.gabriel2q@gmail.com>
    Co-authored-by: Rafael-gc <rafaelfernandeszk@gmail.com>
    Co-authored-by: DouglasMonteles <silvadouglas328@gmail.com>

---
+++ Planes/src/points.pl
@@ -0,0 +1,28 @@
+% point( posx, posy, raio, nome).
+point(136,324, 20, 'Rio Branco').
+point(210,298, 20, 'Porto Velho').
+point(286,177, 20, 'Manaus').
+point(523,148, 20, 'Belem').
+point(457,721, 20, 'Porto Alegre').
+point(368,429, 20, 'Cuiaba').
+point(394,529, 20, 'Campo Grande').
+point(508,671, 20, 'Florianopolis').
+point(526,434, 20, 'Brasilia').
+point(498,452, 20, 'Goiania').
+point(543,592, 20, 'Sao Paulo').
+point(496,627, 20, 'Curitiba').
+point(606,579, 20, 'Rio de Janeiro').
+point(657,531, 20, 'Vitoria').
+point(599,520, 20, 'Belo Horizonte').
+point(520,321, 20, 'Palmas').
+point(710,381, 20, 'Salvador').
+point(737,345, 20, 'Aracaju').
+point(764,319, 20, 'Maceio').
+point(782,290, 20, 'Recife').
+point(783,272, 20, 'Joao Pessoa').
+point(776,245, 20, 'Natal').
+point(711,202, 20, 'Fortaleza').
+point(633,222, 20, 'Teresina').
+point(606,183, 20, 'Sao Luis').
+point(464,112, 20, 'Macapa').
+point(272,61, 20, 'Boa Vista').
