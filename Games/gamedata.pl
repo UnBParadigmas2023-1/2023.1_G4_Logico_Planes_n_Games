d204ccf33751ad21af101c599373031fef4e557b
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2023-06-01 13:07:23 -0300

    Adição de mais dados do game

--- Games/gamedata.pl
+++ Games/gamedata.pl
@@ -1,3 +1,4 @@
+% Todas as salas
 room(car).
 room(driveway).
 room(house).
@@ -5,10 +6,18 @@ room(yard).
 room(hallway).
 room('living room').
 room(kitchen).
-room(stairs).
+room('utility room').
+room('stairwell landing').
 room('sisters room').
 room('my room').
 room(bathroom).
+% Localização dos itens
 located(glovebox,car).
 located(key,glovebox).
 located('note from dad',glovebox).
+located(generator,yard).
+located('light switch',hallway).
+located(box,'my room').
+located(pictures,'stairwell landing').
+located(wardrobe,'sisters room').
+located(wardrobe,photographs).
