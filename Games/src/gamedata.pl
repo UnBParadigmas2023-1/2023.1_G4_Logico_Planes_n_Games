b0e03b0ca45fd431072ae0109e3f13808b81111b
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2023-06-02 04:47:52 -0300

    changing some game data

--- Games/src/gamedata.pl
+++ Games/src/gamedata.pl
@@ -10,11 +10,12 @@ room('stairwell landing').
 room('sisters room').
 room('my room').
 room(bathroom).
+room(frontdoor).
 
 % Localização dos itens
-located('glove box',car).
-located(key,'glove box').
-located('note from dad','glove box').
+located(glovebox,car).
+located(key,car).
+located(note,car).
 located(generator,yard).
 located('light switch',hallway).
 located(box,'my room').
@@ -23,12 +24,18 @@ located(wardrobe,'sisters room').
 located(wardrobe,photographs).
 
 % As conexões em cada sala
-door(car,house).
-door(house,car).
-door(house,yard).
-door(house,hallway).
+door(car,frontdoor).
+door(frontdoor, car).
+door(frontdoor, house).
+door(frontdoor, yard).
+door(yard, frontdoor).
+door(house,frontdoor).
 door(yard,house).
-door(hallway,house).
+door(house, yard).
+door(house,'living room').
+door(house,kitchen).
+door(house,'stairwell landing').
+door(hallway, yard).
 door(hallway,'living room').
 door(hallway,kitchen).
 door(hallway,'stairwell landing').
@@ -47,17 +54,15 @@ door(bathroom,'stairwell landing').
 interaction(note, 'The note is from your Dad and reads:\n\nHi Son, hope you enjoy your break. Youl\'ll need to fire up the generator around back to get power and lights on.\n\nAlso, found something in the attic for you, it\'s in your room... Enjoy!').
 interaction(generator, 'You switch the generator on, and it whirrs to life. The house is still dark.').
 interaction('light switch', 'You flick the switch and the lights come on...').
-interaction(wardrobe, 'It is empty, apart from a pile of scattered photographs at the bottom.').
 interaction(box, 'You unwrap the gift excitedly!...\n\nYou can\'t belive it!\n\nDad has found your old computer, a Futuro 128k +2! It\'s been preserved well in the attic and hopefully still works!').
 interaction(computer, 'You start to plug in the various cables and leads...\n\nThe Computer is all set up and ready to go. There is a game here too...').
 interaction(game, 'You put the cassette in the computer, and press play.').
-interaction('glove box', 'Inside is a key, and a handwritten note from Dad. You take both.').
 
 % Texto padrão da localização
 text(car,'You pull up to the driveway of the family holiday home and park the car. It\'s dark, but it\'s as idyllic as you remember from all that time ago. You remember being told to look in the glove box before going in.\n\nIts good be back.').
-text(house,'The house is grand, sat perfectly amongst the trees.\n\nIn front of you is the front door, and the yard stretches around the side of the house.').
+text(frontdoor,'The house is grand, sat perfectly amongst the trees.\n\nIn front of you is the front door, and the yard stretches around the side of the house.').
 text(yard,'The yard has been well maintained. You spent a lot of time here with your family on holiday trips. Good memories.').
-text(hallway,'You enter the house to the hallway. It\'s dark and you can\'t see anything. You feel a light switch next to the door however.'). % luzes apagadas.
+text(house,'You enter the house to the hallway. It\'s dark and you can\'t see anything. You feel a light switch next to the door however.'). % luzes apagadas.
 text(hallway,'The hallway in now brightly lit. There is access to the kitchen and living room here, as well as a set of stairs going up.'). % luzes acesas.
 text('living room', 'A spacious and comfortable living room. We spenta lot of good time in here playing board games with the family. Warm and inviting.').
 text(kitchen,'The kitchen is tidy and well kept. There is a door to a utility room, but otherwise, it\' just a kitchen.').
@@ -69,7 +74,7 @@ text('my room','Your old bedroom. So many good memories in here, and it\'s been 
 
 % o resultado do look around em cada posição.
 look_around(car,'It\'s a nice clean car. Not much to see, but there is a glove box.').
-look_around(house,'The trees strech up to the night sky. There is the house and the yard.').
+look_around(frontdoor,'The trees strech up to the night sky. There is the house and the yard.').
 look_around(yard,'You see a generator on the back wall of the house').
 look_around(hallway,'The hallway is a pleasant welcome to the home. Airy and fresh.').
 look_around('living room', 'There is nothing here of interest.').
@@ -80,12 +85,19 @@ look_around('sisters room','There is also a large wardrobe in the corner of the 
 look_around('my room','It\'s a standard bedroom. A desk, a woodgrained TV. The usual.').
 
 % o resultado do "look at" de cada objeto
-look(house,'The lights are all off, no one else is here.').
-look(yard, 'The spacious yard extends around to the back of the house. A great place to relax in the long summers.').
-look(pictures, 'Family photos and holiday snaps. Our happy family.').
-look(wardrobe, 'A large clothes wardrobe. Unusually, the door is slightly ajar.\n\nShe never liked you going through he stuff.').
-look('wardrobe photographs', 'They are all identical. A forest road at night. You put them back.').
-look(box, 'A large gift-wrapped present. The tag says your name.').
-look('glove box', 'The glove box is closed but appears to be unlocked.').
+can_look_at(house,'The lights are all off, no one else is here.').
+can_look_at(yard, 'The spacious yard extends around to the back of the house. A great place to relax in the long summers.').
+can_look_at(pictures, 'Family photos and holiday snaps. Our happy family.').
+can_look_at(wardrobe, 'A large clothes wardrobe. Unusually, the door is slightly ajar.\n\nShe never liked you going through he stuff.').
+can_look_at('wardrobe photographs', 'They are all identical. A forest road at night. You put them back.').
+can_look_at(box, 'A large gift-wrapped present. The tag says your name.').
+can_look_at(glovebox, 'The glove box is closed but appears to be unlocked.').
+can_look_inside(glovebox, 'Inside is a key, and a handwritten note from Dad. You take both.').
+can_look_inside(wardrobe, 'It is empty, apart from a pile of scattered photographs at the bottom.').
 
+can_open(frontdoor).
+can_open(box).
+can_read(note).
+can_turn_on(computer).
+can_turn_on(generator).
 
