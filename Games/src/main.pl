4b14c10f2307a904e19bd04cc9a7ae00cf721bea
Victor Eduardo <victorear05@gmail.com>
2023-06-01 21:42:12 -0300

    regras de movimentação

--- Games/src/main.pl
+++ Games/src/main.pl
@@ -6,11 +6,12 @@
 % acao(nome_do_alvo) - Interage com um objeto alvo (no momento apenas acao(mesa) para o quarto, e acao(pia) estao disponiveis).
 
 :- dynamic posicao/1.
-posicao(quarto).
+posicao(carro).
 
 :- nl,write('Bem vindo ao jogo!!'),nl.
 :- write('Para ver o tutorial, digite help.'),nl,nl.
 
+:- discontiguous game/0.
 
 game :-
     posicao(Pos),
@@ -26,44 +27,72 @@ acao(X):-
     interacao(X, Y), 
     nl,write(Y),nl.
 
-ir(X):- 
+ir(X) :-
     posicao(Y),
-    acessivel(Y, X),
-    retract(posicao(Y)),
-    assert(posicao(X)),
-    posicao(Pos),
-    texto(Pos,Out),
-    nl,nl,write(Out),nl.
+    (   door(Y, X)
+    ->  retract(posicao(Y)),
+        assert(posicao(X)),
+        posicao(Pos),
+        texto(Pos, Out),
+        nl, nl, write(Out), nl
+    ;   write('Você tentou se movimentar para um lugar inacessível.'),
+        nl, nl
+    ).
 
 
 texto(quarto, 'Voce esta no quarto e voce ve uma cama e uma mesa.').
-texto(banheiro, 'Voce chega no banheiro e ve uma pia ').
+texto(banheiro, 'Voce chega no banheiro e ve uma pia.').
+texto(quarto, 'Você está no quarto e ASEDFASDFASDF.').
+texto(banheiro, 'Você está no banheiro e ASEDFASDFASDF.').
+texto(carro, 'Você está no carro e ASEDFASDFASDF.').
+texto(casa, 'Você está na casa e ASEDFASDFASDF.').
+texto(jardim, 'Você está no jardim e ASEDFASDFASDF.').
+texto(hall_entrada, 'Você está no hall_entrada e ASEDFASDFASDF.').
+texto(sala_de_estar, 'Você está na sala_de_estar e ASEDFASDFASDF.').
+texto(segundo_andar, 'Você está no segundo_andar e ASEDFASDFASDF.').
+texto(cozinha, 'Você está na cozinha e ASEDFASDFASDF.').
+texto(dispensa, 'Você está na dispensa e ASEDFASDFASDF.').
+texto(seu_quarto, 'Você está no seu_quarto e ASEDFASDFASDF.').
+texto(quarto_da_irma, 'Você está no quarto_da_irma e ASEDFASDFASDF.').
+texto(carro, 'Você está no carro e ASEDFASDFASDF.').
 
 texto_acao(quarto, 'Acoes: acao(mesa), acao(cama).').
 texto_acao(banheiro, 'Acoes: acao(pia)').
 
-acessivel(carro, casa).
-acessivel(casa, carro).
-acessivel(casa, jardim).
-acessivel(jardim, casa).
-acessivel(casa, hall_entrada).
-acessivel(hall_entrada, casa).
-acessivel(hall_entrada, sala_de_estar).
-acessivel(sala_de_estar, hall_entrada).
-acessivel(hall_entrada, segundo_andar).
-acessivel(cozinha, hall_entrada).
-acessivel(hall_entrada, cozinha).
-acessivel(cozinha, dispensa).
-acessivel(dispensa, cozinha).
-acessivel(seu_quarto, segundo_andar).
-acessivel(segundo_andar, seu_quarto).
-acessivel(quarto_da_irma, segundo_andar).
-acessivel(segundo_andar, quarto_da_irma).
-acessivel(segundo_andar, banheiro).
-acessivel(banheiro, segundo_andar).
+door(carro, casa).
+door(casa, carro).
+door(casa, jardim).
+door(jardim, casa).
+door(casa, hall_entrada).
+door(hall_entrada, casa).
+door(hall_entrada, sala_de_estar).
+door(sala_de_estar, hall_entrada).
+door(hall_entrada, segundo_andar).
+door(cozinha, hall_entrada).
+door(hall_entrada, cozinha).
+door(cozinha, dispensa).
+door(dispensa, cozinha).
+door(seu_quarto, segundo_andar).
+door(segundo_andar, seu_quarto).
+door(quarto_da_irma, segundo_andar).
+door(segundo_andar, quarto_da_irma).
+door(segundo_andar, banheiro).
+door(banheiro, segundo_andar).
 
 interacao(mesa, 'Voce encontra uma chave').
 interacao(pia, 'Voce liga e desliga a torneira, vendo que ela esta funcionando').
 
+lugar(carro).
 lugar(banheiro).
 lugar(quarto).
+lugar(casa).
+lugar(jardim).
+lugar(hall_entrada).
+lugar(sala_de_estar).
+lugar(segundo_andar).
+lugar(cozinha).
+lugar(dispensa).
+lugar(seu_quarto).
+lugar(quarto_da_irma).
+
+game.
