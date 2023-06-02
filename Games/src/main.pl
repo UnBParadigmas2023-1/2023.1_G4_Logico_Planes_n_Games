9afcc8557c35cee2253e21a8f683b450fc4c515c
Victor Eduardo <victorear05@gmail.com>
2023-06-02 03:54:16 -0300

    Adicionando Restrições

--- Games/src/main.pl
+++ Games/src/main.pl
@@ -10,6 +10,21 @@
 :- dynamic position/1.
 position(car).
 
+:- dynamic have_key/1.
+have_key(false).
+
+:- dynamic generator_on/1.
+generator_on(false).
+
+:- dynamic light_on/1.
+light_on(false).
+
+:- dynamic gift_opened/1.
+gift_opened(false).
+
+:- dynamic computer_on/1.
+computer_on(false).
+
 :- tty_clear.
 :- write('You pull up to the driveway of the family holiday home and park the car. It\'s dark, but it\'s as idyllic as you remember from all that time ago. You remember being told to look in the glove box before going in.\n\nIts good be back.').
 
